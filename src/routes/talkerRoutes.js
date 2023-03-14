e5dea6f210b5481b9c1bdc2bcf87a31eef272ff4
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-03-14 19:35:30 -0300

    req 2

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -13,4 +13,16 @@ router.get('/', async (req, res) => {
   return res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile(TALKER_FILE);
+  const talkerById = talkers.find((talker) => Number(talker.id) === Number(id));
+  if (!talkerById) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    }); 
+  }
+  return res.status(200).json(talkerById);
+});
+
 module.exports = router;
